9bec0361172552797c00ecf4fdccaae81dd90bf5
PRIYANSHU <4prianshu2051@gmail.com>
2023-01-05 18:57:18 +0530

    c

--- java_prog/main.java
+++ java_prog/main.java
@@ -1,7 +1,25 @@
-/*
-java is class based and object oriented programming language
-java development kit
+/*Getting Started with java
 
-
+Every line of code that runs in Java must be inside a class.
+java is case-sensitive
+every Java program has a class name which must match the filename, and 
+that every program must contain the main() method.
+each code statement must end with a semicolon (;).
 */
 
+//A basic java programme
+//having a class ,name matches to file-name
+//inside it main method(function in class called methods) 
+//inside it our program is executed.
+public class main {
+    public void main(String[] args ){
+        System.out.println("hello everyone");
+    }
+}
+
+/*
+
+
+
+
+ */
